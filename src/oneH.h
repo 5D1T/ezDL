1ff6c061739cd3bcaf13ebf00baaf3da8aec6bfd
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-18 08:05:43 +0100

    Update oneH.h

--- src/oneH.h
+++ src/oneH.h
@@ -1,13 +1,22 @@
+/*
+** oneH.h
+** A header file including all the necessery header files
+*/
+
+
 #ifndef EZDL_ONEH_H
 #define EZDL_ONEH_H
 
 
+// stdlib
 #include <iostream>
 #include <stdlib.h>
 
+// SDL
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_ttf.h"
 
+// ezDL
 #include "window.h"
 #include "GUIelement.h"
 #include "button.h"
