b0ca6467f2fa3af367c04e210f8c84ebc4e024a5
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-18 18:31:02 +0100

    Update sound.cpp

--- tests/sound/sound.cpp
+++ tests/sound/sound.cpp
@@ -28,8 +28,8 @@ int main(int argc, char** argv)
 		window.clearScreen();
 		// ...
 		while(SDL_PollEvent(&e) != 0)
-        	if(e.type == SDL_QUIT)
-            	quit = true;		
+        		if(e.type == SDL_QUIT)
+            			quit = true;		
 	}
 	
 	SDL_Quit();
