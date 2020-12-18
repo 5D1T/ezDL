364538c2ba0c3ee283049a28584001cd9434eebd
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-18 07:35:06 +0100

    Update window.h

--- window.h
+++ window.h
@@ -1,53 +1,54 @@
 #ifndef WINDOW_H_INCLUDED
 #define WINDOW_H_INCLUDED
-using namespace std;
 
-#endif // WINDOW_H_INCLUDED
 
 class Window
 {
-
-
 private:
-    bool quit = false;
     friend class GUIelement;
-    SDL_Event e;
-    SDL_Rect screenrect;
+    SDL_Event m_event;
+    SDL_Rect m_screenrect;
+    
 public:
-    SDL_Window* s1 = NULL;
-     SDL_Surface* screen = NULL;
-
-
+    SDL_Window* wh = NULL;
+    SDL_Surface* screen = NULL;
 
     Window(int x,int y, int w, int h, const char* name)
     {
-        screenrect.x = x;
-        screenrect.y = y;
-        screenrect.w = w;
-        screenrect.h = h;
-        s1 = SDL_CreateWindow( name, screenrect.x, screenrect.y, screenrect.w, screenrect.h, SDL_WINDOW_SHOWN );
-        if (s1 == NULL)
+        m_screenrect.x = x;
+        m_screenrect.y = y;
+        m_screenrect.w = w;
+        m_screenrect.h = h;
+        wh = SDL_CreateWindow(name, screenrect.x, screenrect.y, screenrect.w, screenrect.h, SDL_WINDOW_SHOWN);
+        if(wh == NULL)
         {
-            cout<<"creating window failed"<<endl;
+            // Errror: Couldn't create the window
+            std::cout << "Error: Couldn't create the window" << std::endl;
         }
     }
 
     void Update(bool &quitflag)
     {
-        screen = SDL_GetWindowSurface(s1);
-        SDL_FillRect(screen,NULL,SDL_MapRGB(screen->format,255,255,255));
+        screen = SDL_GetWindowSurface(wh);
+        SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255,255,255));
 
-        while(SDL_PollEvent(&e) != 0)
+        while(SDL_PollEvent(&m_event) != 0)
         {
-            if(e.type == SDL_QUIT)
+            /*
+            if(m_event.type == SDL_QUIT)
             {
                 quitflag = true;
             }
+            */
+            quitflag = m_event.type == SDL_QUIT;
         }
     }
 
     void ClearScreen()
     {
-        SDL_UpdateWindowSurface(s1);
+        SDL_UpdateWindowSurface(wh);
     }
 };
+
+
+#endif
