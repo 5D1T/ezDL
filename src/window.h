a031721aea369b7e366f6d42c6e21d71c43681b4
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:59:04 +0100

    Update window.h

--- src/window.h
+++ src/window.h
@@ -5,37 +5,39 @@
 class EZDL_Window
 {
 private:
+    SDL_Window* m_wh = NULL;
     SDL_Rect m_screenrect;
 
 public:
-    SDL_Window* wh = NULL;
     SDL_Surface* screen = NULL;
 
     EZDL_Window(int x, int y, int w, int h, const char* name)
-    {
-        m_screenrect = {x, y, w, h};
-        wh = SDL_CreateWindow(name, x, y, w, h, SDL_WINDOW_SHOWN);
-        EZDL_Assert(wh != NULL, "Couldn't create the window");
-    }
+	{
+	    m_screenrect = {x, y, w, h};
+	    m_wh = SDL_CreateWindow(name, x, y, w, h, SDL_WINDOW_SHOWN);
+	    EZDL_Assert(m_wh != NULL, "Couldn't create the window");
+	}
 
-    void update(SDL_Event &e, bool &quit)
-    {
-        screen = SDL_GetWindowSurface(wh);
-        SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
 
-        while(SDL_PollEvent(&e))
-        {
-            if(e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
-        }
-    }
+	void update(SDL_Event &e, bool &quit)
+	{
+	    screen = SDL_GetWindowSurface(m_wh);
+	    SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
 
-    void clearScreen()
-    {
-        SDL_UpdateWindowSurface(wh);
-    }
+	    while(SDL_PollEvent(&e))
+	    {
+	        if(e.type == SDL_QUIT)
+	        {
+	            quit = true;
+	        }
+	    }
+	}
+
+
+	void clearScreen()
+	{
+	    SDL_UpdateWindowSurface(m_wh);
+	}
 };
 
 
