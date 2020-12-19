8f56091477a79bc89c42dc98af95f6486c4410e4
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:57:26 +0100

    Update button.h

--- src/button.h
+++ src/button.h
@@ -1,9 +1,6 @@
 #ifndef EZDL_BUTTON_H
 #define EZDL_BUTTON_H
 
-#include <iostream>
-#include <thread>
-
 
 class EZDL_Button: public EZDL_Sprite
 {
@@ -11,22 +8,24 @@ private:
     bool m_click = false;
 
 public:
-    void update(const SDL_Event &e, bool &state, SDL_Surface* screen)
-    {
-        switch(e.type)
-        {
-        case SDL_MOUSEBUTTONDOWN:
-            m_click = true;
-            break;
-        case SDL_MOUSEBUTTONUP:
-            state = (m_click &&
-                    (e.button.x >= m_params.x && e.button.x <= m_params.x + m_params.w) &&
-                    (e.button.y >= m_params.y && e.button.y <= m_params.y + m_params.h));
-            m_click = false;
-            break;
-        }
-        SDL_BlitSurface(m_texture, &m_params, screen, &m_params);
-    }
+    using EZDL_Sprite::EZDL_Sprite;
+
+    void update(SDL_Surface* screen, SDL_Event &e, bool &state)
+	{
+	    switch(e.type)
+	    {
+	    case SDL_MOUSEBUTTONDOWN:
+	        m_click = true;
+	        break;
+	    case SDL_MOUSEBUTTONUP:
+	        state = (m_click &&
+	                (e.button.x >= m_params.x && e.button.x <= m_params.x + m_params.w) &&
+	                (e.button.y >= m_params.y && e.button.y <= m_params.y + m_params.h));
+	        m_click = false;
+	        break;
+	    }
+	    SDL_BlitSurface(m_texture, &m_params, screen, &m_params);
+	}
 };
 
 
