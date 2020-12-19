aa91ecdc1c2ec808aac2b6249bb1d3089291bb41
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 12:43:08 +0100

    Update and rename UIelement.h to sprite.h

---
+++ src/sprite.h
@@ -0,0 +1,24 @@
+#ifndef EZDL_SPRITE_H
+#define EZDL_SPRITE_H
+
+#include "error.h"
+#include <iostream>
+
+
+class EZDL_Sprite
+{
+protected:
+    SDL_Rect m_params;
+    SDL_Surface* m_texture = NULL;
+
+public:
+    void init(int x, int y, int w, int h, const char* path)
+    {
+        m_params = {x, y, w, h};
+        m_texture = SDL_LoadBMP(path);
+        EZDL_Assert(m_texture != NULL, "Texture of a Sprite is NULL", "The .bmp file is either corrupted or doesn't exist");
+    }
+};
+
+
+#endif
