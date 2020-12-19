8e77be2f08af64a1d16cb7f2b703b6e86837905f
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:58:28 +0100

    Update sprite.h

--- src/sprite.h
+++ src/sprite.h
@@ -1,9 +1,6 @@
 #ifndef EZDL_SPRITE_H
 #define EZDL_SPRITE_H
 
-#include "error.h"
-#include <iostream>
-
 
 class EZDL_Sprite
 {
@@ -12,12 +9,12 @@ protected:
     SDL_Surface* m_texture = NULL;
 
 public:
-    void set(int x, int y, int w, int h, const char* path)
+    EZDL_Sprite(int x, int y, int w, int h, const char* texture)
     {
-        m_params = {x, y, w, h};
-        m_texture = SDL_LoadBMP(path);
-        EZDL_Assert(m_texture != NULL, "Texture of a Sprite is NULL", "The .bmp file is either corrupted or doesn't exist");
-    }
+	    m_params = {x, y, w, h};
+	    m_texture = SDL_LoadBMP(texture);
+	    EZDL_Assert(m_texture != NULL, "Texture of a Sprite is NULL", "The .bmp file is either corrupted or doesn't exist");
+	}
 };
 
 
