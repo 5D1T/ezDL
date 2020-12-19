a31838737f14834189ed1ebd972420da5fb8103b
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:57:09 +0100

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -2,39 +2,47 @@
 #define EZDL_AUDIO_H
 
 #include <cstddef>
-#include <iostream>
 #include <thread>
 
 
 class EZDL_Audio
 {
 private:
-    static void playSound(const char* path, size_t lenght)
+    size_t m_length;
+    const char* m_path;
+
+    static void playSound(const char* path, size_t length)
     {
-        SDL_AudioSpec soundSpec;
-        Uint8 *soundBuffer;
-        Uint32 soundLength = lenght;
+	    SDL_AudioSpec soundSpec;
+	    Uint8 *soundBuffer;
+	    Uint32 soundLength = length;
 
-        SDL_LoadWAV(path, &soundSpec, &soundBuffer, &soundLength);
-        SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &soundSpec, NULL, 0);
+	    SDL_LoadWAV(path, &soundSpec, &soundBuffer, &soundLength);
+	    SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &soundSpec, NULL, 0);
 
-        int a = SDL_QueueAudio(deviceId, soundBuffer, soundLength);
-        SDL_PauseAudioDevice(deviceId, 0);
-        SDL_Delay(lenght);
+	    int a = SDL_QueueAudio(deviceId, soundBuffer, soundLength);
+	    SDL_PauseAudioDevice(deviceId, 0);
+	    SDL_Delay(length);
 
-        EZDL_Assert(a);
+	    EZDL_Assert(a);
 
-        SDL_CloseAudioDevice(deviceId);
-        SDL_FreeWAV(soundBuffer);
-    }
+	    SDL_CloseAudioDevice(deviceId);
+	    SDL_FreeWAV(soundBuffer);
+	}
     
 public:
-    void play(const char* path, size_t lenght)
+    EZDL_Audio(const char* path, size_t length)
     {
-        std::thread t;
-        t = std::thread(playSound, path, lenght);
-        t.detach();
-    }
+		m_path = path;
+		m_length = length;
+	}
+
+    void play()
+	{
+	    std::thread t;
+	    t = std::thread(playSound, m_path, m_length);
+	    t.detach();
+	}
 };
 
 
