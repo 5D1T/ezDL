02ad1b0b1b37d038e093f64d06439b8c6c84c67d
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:58:46 +0100

    Update text.h

--- src/text.h
+++ src/text.h
@@ -2,43 +2,42 @@
 #define EZDL_TEXT_H
 
 #include <cstddef>
-#include <iostream>
 
 
 class EZDL_Text
 {
 private:
-    SDL_Color m_color = {255,255,255};
-    SDL_Surface* m_textSurface = NULL;
     SDL_Rect m_params;
     TTF_Font* m_ttffont = NULL;
+    SDL_Surface* m_textSurface = NULL;
+    SDL_Color m_color = {255, 255, 255};
 
 public:
-    EZDL_Text(SDL_Color color, size_t fontsize, const char* font, SDL_Rect params)
-    {
-        m_color = color;
-        m_ttffont = TTF_OpenFont(font, fontsize);
+	EZDL_Text(size_t font_size, const char* font, SDL_Color color, SDL_Rect params)
+	{
+	    m_color = color;
+	    m_ttffont = TTF_OpenFont(font, font_size);
 
-        EZDL_Assert(TTF_OpenFont(font, fontsize)                      != NULL, "Couldn't load the font file");
-        EZDL_Assert(TTF_RenderText_Solid(m_ttffont, "default", color) != NULL, "Couldn't render the text");
-        
-       m_params = params;
-    }
+	    EZDL_Assert(TTF_OpenFont(font, font_size)                     != NULL, "Couldn't load the font file");
+	    EZDL_Assert(TTF_RenderText_Solid(m_ttffont, "default", color) != NULL, "Couldn't render the text");
+	    
+	    m_params = params;
+	}
 
-    void update(const char* text, SDL_Surface* screen, bool visible)
-    {
-        if(visible)
-        {
-            m_textSurface = TTF_RenderText_Solid(m_ttffont, text, m_color);
-            SDL_BlitSurface(m_textSurface, NULL, screen, &m_params);
-        }
-    }
+	~EZDL_Text()
+	{
+		TTF_CloseFont(m_ttffont);
+	    SDL_FreeSurface(m_textSurface);
+	}
 
-    void free()
-    {
-        TTF_CloseFont(m_ttffont);
-        SDL_FreeSurface(m_textSurface);
-    }
+	void update(SDL_Surface* screen, const char* text, bool visible)
+	{
+	    if(visible)
+	    {
+	        m_textSurface = TTF_RenderText_Solid(m_ttffont, text, m_color);
+	        SDL_BlitSurface(m_textSurface, NULL, screen, &m_params);
+	    }
+	}
 };
 
 
