58cbf68529795e26fbdb5189ea5997c32f9dc291
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 12:42:50 +0100

    Update window.h

--- src/window.h
+++ src/window.h
@@ -1,7 +1,7 @@
 #ifndef EZDL_WINDOW_H
 #define EZDL_WINDOW_H
 
-#include <cstddef>
+#include "error.h"
 
 
 class EZDL_Window
@@ -9,25 +9,19 @@ class EZDL_Window
 private:
     SDL_Event m_event;
     SDL_Rect m_screenrect;
-    
+
 public:
     SDL_Window* wh = NULL;
     SDL_Surface* screen = NULL;
 
-    EZDL_Window(size_t x, size_t y, size_t w, size_t h, const char* name)
+    EZDL_Window(int x, int y, int w, int h, const char* name)
     {
-        m_screenrect.x = x;
-        m_screenrect.y = y;
-        m_screenrect.w = w;
-        m_screenrect.h = h;
-        wh = SDL_CreateWindow(name, m_screenrect.x, m_screenrect.y, m_screenrect.w, m_screenrect.h, SDL_WINDOW_SHOWN);
-        if(wh == NULL)
-        {
-            std::cout << "Error: Couldn't create the window" << std::endl;
-        }
+        m_screenrect = {x, y, w, h};
+        wh = SDL_CreateWindow(name, x, y, w, h, SDL_WINDOW_SHOWN);
+        EZDL_Assert(wh != NULL, "Couldn't create the window");
     }
 
-    void update(bool &quit_flag)
+    void update(bool &quit)
     {
         screen = SDL_GetWindowSurface(wh);
         SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
@@ -36,7 +30,7 @@ public:
         {
             if(m_event.type == SDL_QUIT)
             {
-                quit_flag = true;
+                quit = true;
             }
         }
     }
