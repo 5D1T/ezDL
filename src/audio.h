75ed3c05c5182cb4c9b508ec32c9be9d0581aa3d
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 12:40:44 +0100

    Update audio.h

--- src/audio.h
+++ src/audio.h
@@ -1,6 +1,7 @@
 #ifndef EZDL_AUDIO_H
 #define EZDL_AUDIO_H
 
+#include "error.h"
 #include <cstddef>
 #include <iostream>
 #include <thread>
@@ -13,24 +14,21 @@ private:
     {
         SDL_AudioSpec soundSpec;
         Uint8 *soundBuffer;
-        Uint32 soundLength;
-        soundLength = lenght;
+        Uint32 soundLength = lenght;
 
         SDL_LoadWAV(path, &soundSpec, &soundBuffer, &soundLength);
-
         SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &soundSpec, NULL, 0);
 
         int a = SDL_QueueAudio(deviceId, soundBuffer, soundLength);
         SDL_PauseAudioDevice(deviceId, 0);
         SDL_Delay(lenght);
 
-        if (!a)
-        {
-            std::cout << SDL_GetError() << std::endl;
-        }
+        EZDL_Assert(a);
+
         SDL_CloseAudioDevice(deviceId);
         SDL_FreeWAV(soundBuffer);
     }
+    
 public:
     void play(const char* path, size_t lenght)
     {
