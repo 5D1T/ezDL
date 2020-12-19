ac385d0ebcf15f181cedab1debbd1591675787c2
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 17:58:08 +0100

    Update file.h

--- src/file.h
+++ src/file.h
@@ -1,70 +1,74 @@
 #ifndef EZDL_FILE_H
 #define EZDL_FILE_H
 
-#include "error.h"
-#include <iostream>
 #include <fstream>
 
 
 class EZDL_File
 {
 private:
-    std::ofstream writer;
-    std::ifstream reader;
-    const char* data = NULL;
+    const char* m_path;
+    std::ofstream m_writer;
+    std::ifstream m_reader;
 
 public:
-    char* read(const char* path)
-    {
-        EZDL_Assert(path != NULL, "Path for reading the file is NULL");
-        
-        reader.open(path, std::ifstream::in);
-        if(reader.is_open())
-        {
-            reader.seekg(0, reader.end);
-            int length = reader.tellg();
-            reader.seekg(0, reader.beg);
-            
-            char* buffer = new char[length];
-            reader.read(buffer,length);
+	EZDL_File(const char* path)
+	{
+	    m_path = path;
+	}
 
-            return buffer;
-            delete buffer;
-            reader.close();
-        }
-        else
-        {
-            EZDL_Error("Couldn't open the file for reading");
-        }
-    }
-
-    void write(const char* path, const char* in)
+    char* read()
     {
-        writer.open(path, std::ifstream::out);
-        if(writer.is_open())
-        {
-            writer << in;
-            writer.close();
-        }
-        else
-        {
-            EZDL_Error("Couldn't open the file for writing");
-        }
-    }
+	    EZDL_Assert(m_path != NULL, "Path for reading the file is NULL");
+	    
+	    m_reader.open(m_path, std::ifstream::in);
+	    if(m_reader.is_open())
+	    {
+	        m_reader.seekg(0, m_reader.end);
+	        int length = m_reader.tellg();
+	        m_reader.seekg(0, m_reader.beg);
+	        
+	        char* buffer = new char[length];
+	        m_reader.read(buffer, length);
 
-    void clear(const char* path)
-    {
-        writer.open(path, std::ifstream::trunc);
-        if(writer.is_open())
-        {
-            writer.write("", 0);
-            writer.close();
-        }
-        else
-        {
-            EZDL_Error("Couldn't open the file for clearing");
-        }
-    }
+	        return buffer;
+	        delete buffer;
+	        m_reader.close();
+	    }
+	    else
+	    {
+	        EZDL_Error("Couldn't open the file for reading");
+	    }
+	}
+
+    void write(const char* in)
+	{
+	    m_writer.open(m_path, std::ifstream::out);
+	    if(m_writer.is_open())
+	    {
+	        m_writer << in;
+	        m_writer.close();
+	    }
+	    else
+	    {
+	        EZDL_Error("Couldn't open the file for writing");
+	    }
+	}
+
+
+	void clear()
+	{
+	    m_writer.open(m_path, std::ifstream::trunc);
+	    if(m_writer.is_open())
+	    {
+	        m_writer.write("", 0);
+	        m_writer.close();
+	    }
+	    else
+	    {
+	        EZDL_Error("Couldn't open the file for clearing");
+	    }
+	}
 };
 
 
