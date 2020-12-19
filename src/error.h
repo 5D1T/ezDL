fa620241fe9ef1ea6bd0e9081d5d76d10dfccaf5
kvbc <64214044+kvbc@users.noreply.github.com>
2020-12-19 12:41:28 +0100

    Add files via upload

---
+++ src/error.h
@@ -0,0 +1,32 @@
+#ifndef EZDL_ERROR_H
+#define EZDL_ERROR_H
+
+#include <csetjmp>
+#include <iostream>
+
+
+void EZDL_Error(const char* msg = NULL, const char* note = NULL)
+{
+	if(msg != NULL)
+	{
+		std::cout << "EZDL_Error: " << msg << std::endl;
+	}
+	std::cout << "SDL_Error: " << SDL_GetError() << std::endl;
+	if(note != NULL)
+	{
+		std::cout << "Note: " << note << std::endl;
+	}
+}
+
+
+
+void EZDL_Assert(bool expr, const char* msg = NULL, const char* note = NULL)
+{
+	if(!expr)
+	{
+		EZDL_Error(msg, note);
+	}
+}
+
+
+#endif
